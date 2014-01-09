cfac408c01abb30970e3c0e0ec886aa178b7842c
Jeduan Cornejo <jeduan@gmail.com>
2014-01-09 02:22:38 -0600

    Changing templates to make sense for lua

--- app/index.js
+++ app/index.js
@@ -2,16 +2,19 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
-var _ = require('underscore.string');
 
-
-var BowerGenerator = module.exports = function BowerGenerator(args, options, config) {
+var BowerGenerator = module.exports = function BowerGenerator(args, options) {
   yeoman.generators.Base.apply(this, arguments);
 
   this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
+    this.installDependencies({
+      bower: true,
+      npm: false,
+      skipInstall: options['skip-install']
+    });
   });
 
+  // esto se usa?
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
@@ -25,43 +28,29 @@ BowerGenerator.prototype.askFor = function askFor() {
 
   var prompts = [{
     name: 'bowerComponentName',
-    message: "What's the name of your bower component?"
+    message: 'What\'s the name of your bower component?'
   },
   {
     name: 'description',
-    message: 'Provide a short description for your component!'
-  },
-  {
-    name: 'livereloadPort',
-    message: 'Which port do you wish to use for livereloading?'
+    message: 'Provide a short description for your component'
   }];
 
   this.prompt(prompts, function (props) {
     this.bowerComponentName = props.bowerComponentName;
-    this.slug = _.slugify(this.bowerComponentName);
+    this.slug = this._.slugify(this.bowerComponentName);
     this.description = props.description;
-    this.livereloadPort = props.livereloadPort;
-    this.validVariableName = _.capitalize(_.slugify(this.bowerComponentName)).replace('-','');
+    this.validVariableName = this._.classify(this.bowerComponentName);
 
     cb();
   }.bind(this));
 };
 
 BowerGenerator.prototype.app = function app() {
-  this.mkdir('test');
-  this.mkdir('src');
-  this.mkdir('examples');
-  this.componentName = _.slugify(this.bowerComponentName);
-
-  this.template('_bower-component.coffee', 'src/' + this.componentName + '.coffee');
-  this.template('_bower-component-tests.coffee', 'test/' + this.componentName + '-tests.coffee');
-  this.template('_Gruntfile.coffee', 'Gruntfile.coffee');
-  this.copy('_package.json', 'package.json');
+  this.template('_bower-component.lua', this.slug + '.lua');
   this.template('_bower.json', 'bower.json');
-  this.copy('_index.html', 'examples/index.html');
+  this.template('bowerrc', '.bowerrc');
 };
 
 BowerGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
+  this.copy('gitignore', '.gitignore');
 };
---
+++ app/templates/_bower-component.lua
@@ -0,0 +1,2 @@
+local <%= validVariableName %> = {}
+return <%= validVariableName %>
